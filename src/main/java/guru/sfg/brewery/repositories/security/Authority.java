efe13965be55a4078a6cee94bedff0620dd91320
nazareh <MY_NAME@example.com>
2020-09-03 20:28:22 +0800

    Spring DAta JPA Repositories

---
+++ src/main/java/guru/sfg/brewery/repositories/security/Authority.java
@@ -0,0 +1,10 @@
+/*
+ * Created by Nazareh on 3/9/20, 8:25 pm
+ */
+
+package guru.sfg.brewery.repositories.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Authority extends JpaRepository<Authority,Integer> {
+}
