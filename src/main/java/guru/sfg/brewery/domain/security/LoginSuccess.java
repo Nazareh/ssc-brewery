61287cb63b646ed317ec5d1a30b718d614b8ccf5
nazareh <MY_NAME@example.com>
2020-09-09 16:56:40 +0800

    Persistence of Authentication Success Events

---
+++ src/main/java/guru/sfg/brewery/domain/security/LoginSuccess.java
@@ -0,0 +1,38 @@
+package guru.sfg.brewery.domain.security;
+
+/*
+ *   Created by Nazareh on 9/9/20
+ */
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class LoginSuccess {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    private User user;
+
+    private String sourceIp;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastUpdateDate;
+
+}
