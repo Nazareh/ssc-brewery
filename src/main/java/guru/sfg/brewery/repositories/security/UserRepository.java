ec61c53aaaaec0afbeac0e810fcb533168f16175
nazareh <MY_NAME@example.com>
2020-09-11 16:15:07 +0800

    Automatically Unlocking Accounts After Period of Time

--- src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
@@ -6,9 +6,15 @@ package guru.sfg.brewery.repositories.security;
 
 import guru.sfg.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByAccountNonLockedAndLastUpdateDateIsAfter(Boolean nonLockedAccount, Timestamp timestamp);
+
 }
---
+++ src/main/java/guru/sfg/brewery/security/UserUnlockService.java
@@ -0,0 +1,42 @@
+package guru.sfg.brewery.security;
+
+/*
+ *   Created by Nazareh on 11/9/20
+ */
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserUnlockService {
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
+    public void unlockAccounts(){
+        log.debug("Running Unlock Accounts");
+
+        List<User> lockedUsers = userRepository.findAllByAccountNonLockedAndLastUpdateDateIsAfter(
+                false, Timestamp.valueOf(LocalDateTime.now().minusSeconds(5))
+        );
+
+        if(lockedUsers.size() >0){
+            lockedUsers.forEach( user -> {
+                user.setAccountNonLocked(true);
+                log.debug(user.getUsername() + " Account Unlocked");
+            });
+            userRepository.saveAll(lockedUsers);
+        }
+
+    }
+
+}
