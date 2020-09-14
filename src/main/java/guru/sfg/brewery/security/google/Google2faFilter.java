71f7cf580a766ea543bf2e1abc5bdb5b34c63082
nazareh <MY_NAME@example.com>
2020-09-14 15:30:17 +0800

    Spring Security 2FA Failure Handler

--- src/main/java/guru/sfg/brewery/security/google/Google2faFilter.java
+++ src/main/java/guru/sfg/brewery/security/google/Google2faFilter.java
@@ -24,10 +24,10 @@ import java.io.IOException;
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 public class Google2faFilter extends GenericFilterBean {
 
     private final AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl();
+    private final Google2faFailureHandler google2faFailureHandler = new Google2faFailureHandler();
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -43,15 +43,12 @@ public class Google2faFilter extends GenericFilterBean {
                 User user = (User) authentication.getPrincipal();
                 if(user.getUseGoogle2fa() && user.getGoole2faRequired()) {
                     log.debug("2FA Required");
-                    // todo add failure handler
+
+                    google2faFailureHandler.onAuthenticationFailure(request,response,null);
+                    return;
                 }
             }
         }
-
-
-
         filterChain.doFilter(request,response);
-
-
     }
 }
