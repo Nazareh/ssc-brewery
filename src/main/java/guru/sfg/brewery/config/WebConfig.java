3c46beac8b82c80c006490fbb956455bbf29866c
nazareh <MY_NAME@example.com>
2020-09-15 10:05:02 +0800

    Spring MVC CORS Annotation Config

--- src/main/java/guru/sfg/brewery/config/WebConfig.java
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET","POST","PUT");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("GET","POST","PUT");
+//    }
 }
